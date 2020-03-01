50a7062990e6ae916b12376ae36a49c8180e011f
Patryk <gustaw5022@gmail.com>
2020-03-01 20:10:05 +0100

    added loading bar at logoActivity

--- app/src/main/java/com/e/truehomemobile/Activities/LogoActivity.kt
+++ app/src/main/java/com/e/truehomemobile/Activities/LogoActivity.kt
@@ -1,7 +1,9 @@
 package com.e.truehomemobile.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.e.truehomemobile.R
 
 class LogoActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class LogoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logo)
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        },3700)
+    }
+
+    override fun onBackPressed() {
+
     }
 }
