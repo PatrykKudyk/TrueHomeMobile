cd2edac91e682676a372274264fdc5da381e919f
Patryk <gustaw5022@gmail.com>
2020-03-01 21:34:59 +0100

    fixed loading bar animation

--- app/src/main/java/com/e/truehomemobile/Activities/LogoActivity.kt
+++ app/src/main/java/com/e/truehomemobile/Activities/LogoActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.e.truehomemobile.R
 
 class LogoActivity : AppCompatActivity() {
@@ -11,6 +13,15 @@ class LogoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logo)
+
+        val spinRight = AnimationUtils.loadAnimation(this, R.anim.spinright)
+        spinRight.repeatCount = 10
+        val loadingBar = findViewById(R.id.loadingBar) as ImageView
+
+
+        loadingBar.startAnimation(spinRight)
+
+
         Handler().postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
