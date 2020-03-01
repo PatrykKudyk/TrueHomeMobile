4dfdea510d821980154c223b2b4f8b3ffa515e29
Patryk <gustaw5022@gmail.com>
2020-03-01 20:09:41 +0100

    added delayed loginActivity show

---
+++ app/src/main/java/com/e/truehomemobile/Activities/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.e.truehomemobile.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e.truehomemobile.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    override fun onBackPressed() {
+
+    }
+}
