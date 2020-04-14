45fc5698610c1eb25e84b58c8492512704ab98ad
Patryk <gustaw5022@gmail.com>
2020-04-14 20:48:08 +0200

    Constructor changes

---
+++ app/src/main/java/com/e/truehomemobile/models/apartment/Apartment.kt
@@ -0,0 +1,12 @@
+package com.e.truehomemobile.models.apartment
+
+class Apartment(var apartmentId: Long,
+                var apartmentName: String,
+                var apartmentCity: String,
+                var apartmentStreet: String,
+                var apartmentStreetNumber: String,
+                var apartmentNumber: String,
+                var apartmentZipCode: String,
+                var apartmentPrice: Int,
+                var apartmentDescription: String) {
+}
