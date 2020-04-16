689d2d918cb1f59ea0b65b607c7dca7b237cdfca
Patryk <gustaw5022@gmail.com>
2020-04-16 17:28:28 +0200

    basic not_logged apartment_list_views

--- app/src/main/java/com/e/truehomemobile/activityHolders/logic/ApartmentListLogicHolder.kt
+++ app/src/main/java/com/e/truehomemobile/activityHolders/logic/ApartmentListLogicHolder.kt
@@ -3,7 +3,10 @@ package com.e.truehomemobile.activityHolders.logic
 import android.app.Activity
 import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.e.truehomemobile.R
+import com.e.truehomemobile.activityHolders.MarginItemDecoration
 import com.e.truehomemobile.adapters.ApartmentListAdapter
+import com.e.truehomemobile.models.apartment.Apartment
 import com.e.truehomemobile.models.classes.LogicHolder
 import kotlinx.android.synthetic.main.activity_apartment_list_activitty.*
 
@@ -12,6 +15,24 @@ class ApartmentListLogicHolder(context: Context, activity: Activity) : LogicHold
 
     fun initActivity(){
         activity.apartment_list_recycler_view.layoutManager = LinearLayoutManager(context)
-        activity.apartment_list_recycler_view.adapter = ApartmentListAdapter()
+        activity.apartment_list_recycler_view.addItemDecoration(MarginItemDecoration(12))
+        activity.apartment_list_recycler_view.adapter = ApartmentListAdapter(initApartmentList())
+    }
+
+    private fun initApartmentList(): ArrayList<Apartment>{               // metoda testowa, bez bazy
+        val apartment1 = Apartment(0, "Dobry apartment", "Wroclaw",
+            "Bzowa", "21", "37a", "51-132",
+            300,"Opis apartamentu, który jest właśnie opisywany bla bla" +
+                    "bla bla bla")
+        val apartmentList = ArrayList<Apartment>()
+        apartmentList.add(apartment1)
+        apartmentList.add(apartment1)
+        apartmentList.add(apartment1)
+        apartmentList.add(apartment1)
+        apartmentList.add(apartment1)
+        apartmentList.add(apartment1)
+        apartmentList.add(apartment1)
+
+        return apartmentList
     }
 }
