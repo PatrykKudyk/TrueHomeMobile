f955c2361615f875caf3b2ff2f6aa2b97eb23c15
Patryk <gustaw5022@gmail.com>
2020-04-28 12:29:20 +0200

    created activity + added polish/english language

---
+++ app/src/main/java/com/e/truehomemobile/activities/LanguageActivity.kt
@@ -0,0 +1,13 @@
+package com.e.truehomemobile.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e.truehomemobile.R
+
+class LanguageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+    }
+}
