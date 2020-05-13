0a17c73ac21d19cc5bbf6f0d44af01463e417671
Patryk <gustaw5022@gmail.com>
2020-05-14 00:39:42 +0200

    package changes

---
+++ app/src/main/java/com/e/truehomemobile/adapters/page/BigImagesPagerAdapter.kt
@@ -0,0 +1,4 @@
+package com.e.truehomemobile.adapters
+
+class BigImagesPagerAdapter {
+}
--- app/src/main/java/com/e/truehomemobile/viewHolders/ApartmentImagesViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.e.truehomemobile.viewHolders
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class ApartmentImagesViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-}
